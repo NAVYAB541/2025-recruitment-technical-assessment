84e10b06e60f75d1ece3eca2c08af49d0b9e54bb
Navya Bhutoria <z5438156@student.unsw.edu.au>
2025-03-02 21:04:23 +1100

    Corrected chaos

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -31,23 +31,3 @@ pub struct DataResponse {
   string_len: usize,
   int_sum: i32,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use axum::Json;
-    use serde_json::json;
-
-    #[tokio::test]
-    async fn test_process_data() {
-        let request = Json(DataRequest {
-            data: vec![json!("Hello"), json!(1), json!(5), json!("World"), json!("!")],
-        });
-
-        let (status, Json(response)) = process_data(request).await.into_response();
-
-        assert_eq!(status, StatusCode::OK);
-        assert_eq!(response.string_len, 11);
-        assert_eq!(response.int_sum, 6);
-    }
-}
